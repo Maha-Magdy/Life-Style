8507d9b5f4381b033fbd66eed12cc94902381755
Maha Magdy <mahamagdy3@gmail.com>
2021-07-21 13:58:46 +0200

    Complete the unit test for the category model, and add more validation in the model file

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,5 +6,23 @@ RSpec.describe Category, type: :model do
       new_category = Category.new(name: 'Fashion', priority: 1)
       expect(new_category).to be_valid
     end
+
+    it 'will not be accepted if the given priority not unique' do
+      new_first_category = Category.new(name: 'Fashion', priority: 1)
+      new_first_category.save
+
+      new_second_category = Category.new(name: 'Entertainment', priority: 1)
+
+      expect(new_second_category).to_not be_valid
+    end
+
+    it 'will not be accepted if the given name not unique' do
+      new_first_category = Category.new(name: 'Fashion', priority: 1)
+      new_first_category.save
+
+      new_second_category = Category.new(name: 'Fashion', priority: 2)
+
+      expect(new_second_category).to_not be_valid
+    end
   end
 end
