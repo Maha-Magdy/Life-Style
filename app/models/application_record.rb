f0682e26e1ba06f7f84606e57ce3bc8214300196
Maha Magdy <mahamagdy3@gmail.com>
2021-07-21 13:48:56 +0200

    Fix a bug due to required password for each model apart from the user model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,21 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :trackable, :lockable, :validatable
-
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-  
-  # use this instead of email_changed? for Rails = 5.1.x
-  def will_save_change_to_email?
-    false
-  end
 end
