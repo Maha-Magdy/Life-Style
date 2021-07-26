90fbb7b66206813e1b54b0a0024132d811bfffa7
Maha Magdy <mahamagdy3@gmail.com>
2021-07-27 00:34:16 +0200

    work on the look and feel of the log in, log out, and forget password pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'categories#index'
-
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
@@ -8,6 +7,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: "home#index"
 
+  root 'categories#index'
+  
   resources :articles
   resources :categories
   resources :votes
