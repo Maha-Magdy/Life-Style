261960eb3764343ca3ba4a98a8fef78b2c12f815
Maha Magdy <mahamagdy3@gmail.com>
2021-07-28 20:54:20 +0200

    fix test case in the RSpec article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe 'validations' do
-    it 'will be accepted if all the needed properties provided' do
+    it 'will be not accepted because the provided properties missing the photo, and categries' do
       new_user = User.new(name: 'Maha Magdy', email: 'maha_magdy@gmail.com',
                           password: '123456', password_confirmation: '123456')
       new_user.save
+
+      new_category = Category.new(name: 'Fashion', priority: 1)
+
+      new_category.save
 
       new_article = Article.new(author: new_user, title: 'London Journey',
                                 text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
@@ -13,7 +17,7 @@ RSpec.describe Article, type: :model do
                                         numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
                                         optio, eaque rerum! Provident similique accusantium nemo autem.')
 
-      expect(new_article).to be_valid
+      expect(new_article).to_not be_valid
     end
 
     it 'will not be accepted if the title is not provided' do
